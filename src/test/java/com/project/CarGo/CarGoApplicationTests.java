990846d3f18c13a30455c7e4b3faeb349de05b5c
Kirththiga Murugupillai <kirththiga.murugupillai@mail.mcgill.ca>
2025-09-26 20:28:34 -0400

    first commit

---
+++ src/test/java/com/project/CarGo/CarGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CarGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
