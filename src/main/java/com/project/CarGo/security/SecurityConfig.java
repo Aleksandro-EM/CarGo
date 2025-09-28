a02147bc452bc71a4495e57adead5a1d413d5135
Kirththiga Murugupillai <kirththiga.murugupillai@mail.mcgill.ca>
2025-09-27 22:18:45 -0400

    Changed logoutSuccessUrl to display alert when user logged out.

--- src/main/java/com/project/CarGo/security/SecurityConfig.java
+++ src/main/java/com/project/CarGo/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/login?logout")
                         .permitAll()
                 );
         return http.build();
